33b426b68dc7ede4c1461443fdfbde0efc67931a
absens-x <absens.x@gmail.com>
2021-03-14 23:49:20 +0300

    change: Tooltip example

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.css';
+import Tooltip from './examples/Tooltip/Tooltip';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <p>
+                Lorem ipsum dolor sit amet <Tooltip text="this is a tooltip">consectetur</Tooltip> adipisicing elit.
+                Magni, repellendus.
+            </p>
+        </div>
+    );
 }
 
 export default App;
--- src/examples/Tooltip/Tooltip.tsx
+++ src/examples/Tooltip/Tooltip.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
-function Tooltip(): React.ReactNode {
-    return <div></div>;
+interface ITooltopProps {
+    children: string;
+    text: string;
 }
+
+interface ITooltopState {
+    opacity: boolean;
+}
+
+const Tooltip: React.FC<ITooltopProps> = ({ children, text = 'What is it?' }) => {
+    const [opacity, setOpacity] = useState<ITooltopState>({ opacity: false });
+    const tooltipTargetRef = useRef(null);
+    const tooltipTextRef = useRef(null);
+
+    const toggle = (): void => {
+        setOpacity({ opacity: !opacity });
+    };
+    return (
+        <div className="tooltip">
+            <div
+                className="tooltip-wrapper"
+                style={{ backgroundColor: 'red' }}
+                onMouseEnter={() => alert()}
+                ref={tooltipTargetRef}
+            >
+                {children}
+            </div>
+            {/* <div className="tooltip-text">{text}</div> */}
+        </div>
+    );
+};
 
 export default Tooltip;
